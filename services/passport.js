3b54bcc0730ca68982b5b9309b4ccd89d107ee6e
Visotoniki <visotoniki9@users.noreply.github.com>
2020-04-07 19:16:23 +0200

    added cookies

--- services/passport.js
+++ services/passport.js
@@ -10,7 +10,13 @@ const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
+});
 
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then((user) => {
+      done(null, user);
+    });
 });
 
 passport.use(new GoogleStrategy({
