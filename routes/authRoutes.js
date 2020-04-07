7589c08ae67cdbd80b85bcaf8260c63cd0abca73
Visotoniki <visotoniki9@users.noreply.github.com>
2020-04-07 14:36:21 +0200

    code refactored

---
+++ routes/authRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const passport = require('passport');
+
+module.exports = (app) => {
+  app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  }));
+
+  app.get('/auth/google/callback', passport.authenticate('google'));
+};
