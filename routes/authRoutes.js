ce38e472701769c9d82ee28d642b7a6987c61ae3
Visotoniki <visotoniki9@users.noreply.github.com>
2020-04-10 15:48:07 +0200

    small fixes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,10 @@
 const passport = require('passport');
 
 module.exports = (app) => {
+  app.get('/', (req, res) => {
+    res.send(req.user);
+  });
+
   app.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email'],
   }));
