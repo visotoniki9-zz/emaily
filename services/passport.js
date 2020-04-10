df9666609f971a7d11fb435782e44f238cf3bd21
Visotoniki <visotoniki9@users.noreply.github.com>
2020-04-10 16:17:04 +0200

    small fixes

--- services/passport.js
+++ services/passport.js
@@ -19,24 +19,27 @@ passport.deserializeUser((id, done) => {
     });
 });
 
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback',
-}, (accessToken, refreshToken, profile, done) => {
-  User.findOne({ googleId: profile.id })
-    .then((existingUser) => {
-      if (!existingUser) {
-        new User({
-          googleId: profile.id,
-          username: profile.displayName,
-          email: profile.emails[0].value,
-          img: profile.photos[0].value,
-        })
-          .save()
-          .then((user) => done(null, user));
-      } else {
-        done(null, existingUser);
-      }
-    });
-}));
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+    proxy: true,
+  }, (accessToken, refreshToken, profile, done) => {
+    User.findOne({ googleId: profile.id })
+      .then((existingUser) => {
+        if (!existingUser) {
+          new User({
+            googleId: profile.id,
+            username: profile.displayName,
+            email: profile.emails[0].value,
+            img: profile.photos[0].value,
+          })
+            .save()
+            .then((user) => done(null, user));
+        } else {
+          done(null, existingUser);
+        }
+      });
+  }),
+);
